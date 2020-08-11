b996aee1ad1e783632fb19854e0b0c9442ecd39b
Lane <lviss@users.noreply.github.com>
2020-08-10 21:57:04 -0700

    When screen > 1300 pixels wide, keep sidenav open and force a max-width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import * as recipeRoutes from '../assets/recipes/recipes.json';
 import { SwUpdate } from '@angular/service-worker';
 import { interval } from 'rxjs';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Meta, Title } from '@angular/platform-browser';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
-  @ViewChild('nav') sidenav;
+  @ViewChild('sidenav') sidenav: MatSidenav;
   dashes = /-/g;
   title = 'recipes';
   recipeNav = (recipeRoutes as any).default;
   isDarkTheme: boolean = false;
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
 
   constructor(
     private router: Router,
     private swUpdate: SwUpdate,
     public snackBar: MatSnackBar,
     private meta: Meta,
-    private titleService: Title
+    private titleService: Title,
+    private changeDetectorRef: ChangeDetectorRef, 
+    private media: MediaMatcher,
   ) { 
     // check for service worker updates
     if (swUpdate.isEnabled) {
@@ -33,12 +39,18 @@ export class AppComponent implements OnInit {
     }
     this.swUpdate.available.subscribe(event => this.promptUpdate());
     this.isDarkTheme = localStorage.getItem('isDarkTheme') === 'true' ?? false;
+
+    // determine if we're on a small screen
+    this.mobileQuery = media.matchMedia('(max-width: 1300px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit(): void {
     this.router.events.subscribe((val) => {
       if (val) {
-        this.sidenav.close();
+        if (this.mobileQuery.matches)
+          this.sidenav.close();
         // update browser title
         let pageTitle = this.router.url.substring(1, this.router.url.length).replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
         if (pageTitle.length < 1) pageTitle = 'List';
@@ -59,5 +71,9 @@ export class AppComponent implements OnInit {
     this.isDarkTheme = checked;
     localStorage.setItem('isDarkTheme', checked);
     this.meta.updateTag({ content: checked ? '#388e3c' : '#7B1FA2' }, 'name=theme-color');
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 }
